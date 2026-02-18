3ca2b1ed4f1fa781b40039fbfbc8e7ef5e109c80
jovandukes <jovandukes0427@gmail.com>
2026-02-18 10:19:21 -0500

    Started the UI class

---
+++ src/main/java/Wikipedia/WIkiUI.java
@@ -0,0 +1,23 @@
+package Wikipedia;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.concurrent.Task;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.List;
+
+public class WikiUI extends Application {
+
+    private final WikipediaClient client = new WikipediaClient();
+    private final RevisionParser parser = new RevisionParser();
+    private final RevisionFormatter formatter = new RevisionFormatter();
+
